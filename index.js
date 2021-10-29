0ba739d41bc4fc6dbb04857bbe7f281b5ac28572
Tudor Ilisoi <tudor.ilisoi@nxp.com>
2021-10-29 15:35:04 +0300

    delayed fetch

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const MAIN_CONTAINER = document.querySelector('#main')
 function replaceMainHTML(htmlString) {
     MAIN_CONTAINER.innerHTML = htmlString
 }
+
+const delay = (millis = 300) => {
+    let timer
+    return new Promise((resolve, reject) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            resolve(true)
+        }, millis);
+    })
+}
+
 // TODO replace with LRU
 const fetchCache = {}
 
 const fetchHTML = async function (path) {
     replaceMainHTML('Loading...')
+    await delay()
     if (fetchCache[path]) {
         console.log("CACHED", path)
         return fetchCache[path]
     }
 
     try {
-        const result = await fetch('./home.html').then(r => r.text())
+        const result = await fetch(path).then(r => r.text())
         return fetchCache[path] = result
     } catch (error) {
         throw error
@@ -26,33 +38,15 @@ const fetchHTML = async function (path) {
 const watchesPage = async () => {
     //fetch
     //import
-    const tpl = `
-    <h1> Watches
-    </h1>
-    `
+    const tpl = await fetchHTML('./watches.html')
     replaceMainHTML(tpl)
 }
 
 
 
 const homePage = async () => {
-    //fetch
-    //import
-    // const tpl = `
-    // <h1> Home
-    // <button data-product-id="55">Click me</button>
-    // </h1>
-    // `
-
-    // const { default: tpl } = await import("./home.html")
 
     const tpl = await fetchHTML('./home.html')
-
-    // const tpl = document.querySelector("#home")
-
-
-    // debugger
-
     replaceMainHTML(tpl)
     // now you can setup event listeners on #main descendants
     const button = document.querySelector('#main button')
@@ -66,7 +60,7 @@ function changeClientRoute(routeStr) {
 }
 
 function main() {
-    window.addEventListener('hashchange', function () {
+    const handleRoute = () => {
         const { hash } = window.location
         console.log('The hash has changed!', hash)
         switch (hash) {
@@ -78,10 +72,11 @@ function main() {
                 watchesPage()
                 break;
         }
-    }, false);
+    }
+    window.addEventListener('hashchange', handleRoute, false);
 
-    //start with the home page
-    homePage()
+    //handle initial route
+    handleRoute()
 }
 
 
